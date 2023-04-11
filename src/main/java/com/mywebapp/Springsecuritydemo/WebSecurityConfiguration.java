b62d6a0865c559ec6355438759ad9fbaa94da980
wiktoria <wiktoriazimnowoda@gmail.com>
2023-04-11 11:50:43 -0700

    init

---
+++ src/main/java/com/mywebapp/Springsecuritydemo/WebSecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.mywebapp.Springsecuritydemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration{
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/admin").hasAuthority("ADMIN")
+                        .requestMatchers("/home").hasAnyAuthority("ADMIN", "USER")
+                        .requestMatchers("/").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin()
+                .loginPage("/login").permitAll();
+
+//                .httpBasic();
+
+
+        return http.build();
+    }
+}
